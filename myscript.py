717ecded44dd56c4cada906385bac7e291052f76
mvankeulen813 <65417810+mvankeulen813@users.noreply.github.com>
2020-12-04 11:47:34 -0500

    Create myscript.py

---
+++ myscript.py
@@ -0,0 +1,10 @@
+def hello_world(myvar):
+    if myvar == 4:
+        print("hello!")
+        return true
+    return false
+
+def run_script():
+    assert hello_world(4) == true
+    
+run_script()
